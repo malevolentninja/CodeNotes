93efd306f61d5888490ee1d5f68c325280925684
Olivia Boertje <malevolentninja@gmail.com>
2017-04-13 12:06:43 +0100

    Create StringMethods.js

---
+++ Javascript/StringMethods.js
@@ -0,0 +1,72 @@
+// 20 string methods
+var stringOne = "I love to learn"
+var stringTwo = "web development"
+
+// charAt()
+console.log(stringOne.charAt(1))
+
+// charCodeAt()
+console.log(stringOne.charCodeAt(1))
+
+// concat()
+console.log(stringOne.concat(stringTwo))
+
+// endsWith()
+console.log(stringOne.endsWith("learn"))
+
+// fromCharCode()
+console.log(String.fromCharCode(114))
+
+//includes()
+console.log(stringTwo.includes("web"))
+
+// indexOf()
+console.log(stringTwo.indexOf("web"))
+
+// lastIndexOf()
+console.log(stringTwo.lastIndexOf("web"))
+
+// match()
+console.log(stringTwo.match(/web/g))
+
+// repeat()
+console.log(stringOne.repeat(3))
+
+// replace()
+console.log(stringTwo.replace(/web/g, "WEB"))
+
+// search()
+console.log(stringTwo.search("web"))
+
+// slice()
+console.log(stringTwo.slice(2, 4))
+
+// split()
+console.log(stringOne.split(" "))
+
+// startsWith()
+console.log(stringOne.startsWith("I"))
+
+// substr()
+console.log(strinTwo.substr(2, 4))
+
+// substring
+console.log(stringTwo.substring(2, 4))
+
+// toLowerCase()
+console.log(stringOne.toLowerCase())
+
+// toUpperCase()
+console.log(stringOne.toUpperCase())
+
+// trim
+var stringThree = "   Go Javascript!   ";
+console.log(stringThree.trim())
+
+
+
+
+
+
+
+
